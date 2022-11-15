1da7bb0b9626e860f9ef70c74646b0843a6348e2
Danbee Kim <kdb2360@gmail.com>
2022-11-15 12:25:51 +0900

    UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get("");
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
